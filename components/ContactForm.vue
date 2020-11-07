8e6799eb739722d0ca3bdf9160e9d07af49982d6
hironorihoriyama <hironori.horiyama@gmail.com>
2020-11-07 21:59:12 +0900

    form追加

---
+++ components/ContactForm.vue
@@ -0,0 +1,17 @@
+<template>
+<form name="contact" method="POST" netlify>
+  <input type="hidden" name="form-name" :value="contact" />
+  <p>
+    <label>お名前 <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>メールアドレス<input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>お問い合わせ内容<textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">送信する</button>
+  </p>
+</form>
+</template>
